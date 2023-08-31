02f1eed614bd0d5dd178f1630a263bbdbaf8be71
riteshsute <suteritesh@gmail.com>
2023-08-31 16:06:16 +0530

    React Components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+function ExpenseItem() {
+    return ( 
+    <div> 
+        <div>
+            <h2> Expenses! </h2>
+            <div> Food </div>
+        </div> 
+        <div> 5000</div>
+        <br> 
+        </br>
+        <div>
+            <div> Petrol </div>
+        </div> 
+        <div> 10000 </div>
+        <br> 
+        </br>
+        <div>
+            <div> Movie </div>
+        </div> 
+        <div> 2000 </div>
+  
+    </div>
+         
+    )
+}
+
+export default ExpenseItem;
+
