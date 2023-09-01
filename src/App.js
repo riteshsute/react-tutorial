6efe33d1504854a9702a2568409ff9d1c046d40e
riteshsute <suteritesh@gmail.com>
2023-09-01 15:12:50 +0530

    Usage of props to send data as parameter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,42 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Food',
+      amount: 2000,
+      date: new Date(2023, 2, 23)
+    },
+    {
+      id: 'e2',
+      title: 'Movie',
+      amount: 5000,
+      date: new Date(2023, 3, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Travel',
+      amount: 10000,
+      date: new Date(2023, 2, 25)
+    }
+  ]
+
+  const expenseItems = expenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> ExpenseItems </h1>
-        <ExpenseItem> </ExpenseItem>
+      {/* <ExpenseItem> </ExpenseItem> */}
+        {expenseItems}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
-function ExpenseItem() {
+function ExpenseItem(props) {
     const ExpenseDate = new Date(2023);
     const ExpenseCategory = 'Food'
     const ExpenseAmount = '1000'
-    const ExpenseLocation = 'Delhi'
     return ( 
     <div className='expense-item'> 
-        <div> {ExpenseDate.toISOString()} </div>
+        {/* <div> {props.Date.toISOString()} </div> */}
         <div className="expense-item_Title">
-            <h3> Category: {ExpenseCategory} </h3>
+            <h3> Category: {props.title} </h3>
         </div> 
-        <div className="expense-item__amount"> Rs: {ExpenseAmount} </div>
+        <div className="expense-item__amount"> Rs: {props.amount} </div>
         <br>
         </br>
-        <div className="expense-item__location"> Place: {ExpenseLocation} </div>
+        {/* <div className="expense-item__location"> Place: {props.date} </div> */}
     </div>    
     ) 
 }
